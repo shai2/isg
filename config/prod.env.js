1815b90357bdbc7bb27ebd0c03802d5ce2b3fe3d
shai2 <464740437@qq.com>
2018-09-07 12:03:41 +0800

    存档

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,5 @@
 module.exports = {
   NODE_ENV: '"production"',
   ENV_CONFIG: '"prod"',
-  BASE_API: '"https://api-prod"'
+  BASE_API: '"192.168.22.251:9999"'
 }
--- config/sit.env.js
+++ config/sit.env.js
@@ -1,5 +1,5 @@
 module.exports = {
   NODE_ENV: '"production"',
   ENV_CONFIG: '"sit"',
-  BASE_API: '"https://api-sit"'
+  BASE_API: '"192.168.22.251:9999"'
 }
